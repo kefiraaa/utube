40c0dee8938fd487aae6250c395d5d89be075d04
kefiraaa <murzynm67@gmail.com>
2021-10-28 11:14:04 +0800

    6

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -14,21 +14,10 @@ from ..utubebot import UtubeBot
 
 def map_btns(pos):
     if pos == 1:
-        button = [[InlineKeyboardButton(text="-->", callback_data="help+2")]]
+            [InlineKeyboardButton(text="Authentication URL", url=url)],
     elif pos == len(tr.HELP_MSG) - 1:
         auth = GoogleAuth(Config.CLIENT_ID, Config.CLIENT_SECRET)
         url = auth.GetAuthUrl()
-        button = [
-            [InlineKeyboardButton(text="<--", callback_data=f"help+{pos-1}")],
-            [InlineKeyboardButton(text="Authentication URL", url=url)],
-        ]
-    else:
-        button = [
-            [
-                InlineKeyboardButton(text="<--", callback_data=f"help+{pos-1}"),
-                InlineKeyboardButton(text="-->", callback_data=f"help+{pos+1}"),
-            ],
-        ]
     return button
 
 
