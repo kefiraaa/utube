f2a0bf2af61bfc0cf130449bd225746fb6418987
kefiraaa <murzynm67@gmail.com>
2021-11-01 00:09:32 +0800

    Update uploader.py

--- bot/helpers/uploader.py
+++ bot/helpers/uploader.py
@@ -69,7 +69,7 @@ class Uploader:
             )
             description = (
                 Config.VIDEO_DESCRIPTION
-                + "\nUploaded to YouTube with https://tx.me/youtubeitbot"
+                + "\n©Klever 2021"
             )[:5000]
             if not Config.UPLOAD_MODE:
                 privacyStatus = "private"
