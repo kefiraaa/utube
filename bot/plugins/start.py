a80cfd5255147785def9770df2ae0fcdea631220
kefiraaa <murzynm67@gmail.com>
2021-11-01 19:15:26 +0800

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,12 +1,5 @@
-import telebot
 from pyrogram import filters as Filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
-
-
-token = '1912432053:AAHt2sGTDlkpc-KL8cYyZVuw-sauqEMRMl8'
-bot = telebot.Telebot(token)
-
-help = '/help'
 
 from ..translations import Messages as tr
 from ..config import Config
@@ -16,6 +9,7 @@ from ..utubebot import UtubeBot
 @UtubeBot.on_message(
     Filters.private
     & Filters.incoming
+    & Filters.command("start")
     & Filters.user(Config.AUTH_USERS)
 )
 async def _start(c: UtubeBot, m: Message):
@@ -24,26 +18,7 @@ async def _start(c: UtubeBot, m: Message):
     await m.reply_text(
         text=tr.START_MSG.format(m.from_user.first_name),
         quote=True,
-        keyboard = telebot.types.ReplyKeyboardMarkup(true)
-        keyboard.row('Help')
-
-        def send(id, text):
-            bot.send_message(id, text, reply_markup = keyboard)
-
-        @bot.message_handler(commands = ['start'])
-        def answer(message):
-            send(message.chat.id, 'Приветствую тебя!')
-
-        @bot.message_handler(content_types = ['text'])
-        def main(message):
-            id = message.chat.id
-            msg = message.text
-
-            if msg == '/help':
-                send(id.help)
-
-            else:
-                send(id, 'Я тебя не понял!')
-          bot.polling(none_stop = True)
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("Join Project Channel!", url="https://t.me/odbots")]]
         ),
     )
